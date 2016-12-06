72445b438f7de2d474576901d0fd49be695fc423
skibool <skiba.g@gmail.com>
2016-12-06 12:33:45 +0100

    Introduce TI DSPLib

---
+++ microdaq/etc/dsplib/dsplib.h
@@ -0,0 +1,45 @@
+#ifndef __TI_DSPLIB__
+#define __TI_DSPLIB__
+
+#include <DSPF_blk_eswap16.h>
+#include <DSPF_blk_eswap32.h>
+#include <DSPF_blk_eswap64.h>
+#include <DSPF_fltoq15.h>
+#include <DSPF_q15tofl.h>
+#include <DSPF_sp_autocor.h>
+#include <DSPF_sp_biquad.h>
+#include <DSPF_sp_bitrev_cplx.h>
+#include <DSPF_sp_blk_move.h>
+#include <DSPF_sp_cfftr2_dit.h>
+#include <DSPF_sp_cfftr4_dif.h>
+#include <DSPF_sp_convol.h>
+#include <DSPF_sp_dotprod.h>
+#include <DSPF_sp_dotp_cplx.h>
+#include <DSPF_sp_fftSPxSP.h>
+#include <DSPF_sp_fircirc.h>
+#include <DSPF_sp_fir_cplx.h>
+#include <DSPF_sp_fir_gen.h>
+#include <DSPF_sp_fir_r2.h>
+#include <DSPF_sp_icfftr2_dif.h>
+#include <DSPF_sp_ifftSPxSP.h>
+#include <DSPF_sp_iir.h>
+#include <DSPF_sp_iirlat.h>
+#include <DSPF_sp_lms.h>
+#include <DSPF_sp_mat_mul.h>
+#include <DSPF_sp_mat_mul_cplx.h>
+#include <DSPF_sp_mat_mul_gemm.h>
+#include <DSPF_sp_mat_mul_gemm_cplx.h>
+#include <DSPF_sp_mat_trans.h>
+#include <DSPF_sp_maxidx.h>
+#include <DSPF_sp_maxval.h>
+#include <DSPF_sp_minerr.h>
+#include <DSPF_sp_minval.h>
+#include <DSPF_sp_vecmul.h>
+#include <DSPF_sp_vecrecip.h>
+#include <DSPF_sp_vecsum_sq.h>
+#include <DSPF_sp_w_vec.h>
+#include <DSP_fir_r8_h16.h>
+#include <DSP_fir_r8_h24.h>
+#include <DSP_fir_r8_h8.h>
+
+#endif /* __TI_DSPLIB__ */ 
